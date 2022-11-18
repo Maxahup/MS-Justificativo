2cedd1f4d43c5dc80f599a6b7ffceb5c3853a0dc
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2022-11-18 15:24:14 -0300

    Creacion varios

---
+++ src/main/java/Service/JustificativoService.java
@@ -0,0 +1,24 @@
+package Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JustificativoService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String rut;
+    private Integer dias_justificados;
+}
